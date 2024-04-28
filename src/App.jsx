ed815fb63343d3d939a167f400659f38167f8524
Aakash Panchal <aakashpanchal7000@gmail.com>
2024-04-28 15:23:42 +0530

    create folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import LocomotiveScroll from "locomotive-scroll";
 import "./App.css";
 
 function App() {
-  return <>Hello World</>;
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/GlobleContextProvider.jsx
@@ -0,0 +1,54 @@
+import React, { createContext, useContext, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [locoScroll, setLocoScroll] = useState(null);
+  const [cursorSettings, setCursorSettings] = useState({
+    size: 1,
+    isSticky: false,
+    color: "#fff",
+    isBlending: true,
+    text: "",
+  });
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+  });
+
+  const updateBounds = (element) => {
+    const rect = element.getBoundingClientRect();
+    setBounds({
+      left: rect.left,
+      top: rect.top,
+      width: rect.width,
+      height: rect.height,
+    });
+  };
+
+  const resetBounds = () => {
+    setBounds({ left: 0, top: 0, width: 0, height: 0 });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        locoScroll,
+        setLocoScroll,
+        setCursorSettings,
+        cursorSettings,
+        bounds,
+        updateBounds,
+        resetBounds,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useContextProvider = () => {
+  return useContext(GlobalContext);
+};
