6876338fbf0831f7e261ed07745511ce303d044a
Aakash Panchal <aakashpanchal7000@gmail.com>
2024-05-08 19:01:48 +0530

    update preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,8 +1,8 @@
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { useContextProvider } from "../utils/GlobleContextProvider";
+import LogoAni from "../assets/video/clever_studio_logo_anim.mp4";
 
 const Container = styled.div`
   background-color: #000;
@@ -12,35 +12,12 @@ const Container = styled.div`
   z-index: 9999999;
   color: #fff;
 
-  .counter {
-    position: fixed;
-    left: 50px;
-    bottom: 130px;
-    display: flex;
-    height: 100px;
-    font-size: 100px;
-    line-height: 102px;
-    clip-path: polygon(0 0, 100% 0, 100% 100px, 0 100px);
-    font-weight: 400;
-    font-weight: 500;
-  }
-
-  .digit,
-  .num1offset1,
-  .num1offset2 {
-    position: relative;
-  }
-
-  .digit {
-    top: -12px;
-  }
-
-  .num1offset1 {
-    right: -12px;
-  }
-
-  .num1offset2 {
-    right: -10px;
+  video {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 10rem;
   }
 
   @media (max-width: 768px) {
@@ -70,18 +47,18 @@ const Container = styled.div`
 
 const Loader = styled.div`
   background-color: #ffffff39;
-  /* border-radius: 0.5rem; */
-  height: 4rem;
-  width: 95%;
+  border-radius: 0.5rem;
+  height: 4px;
+  width: 30%;
   position: absolute;
-  bottom: 5%;
+  top: 56%;
   left: 50%;
-  /* overflow: hidden; */
+  overflow: hidden;
   transform: translateX(-50%);
   .loader_bar {
     width: 0%;
     height: 100%;
-    background-color: #fff;
+    background-color: #c5c5c5;
   }
 `;
 
@@ -89,56 +66,41 @@ const Preloader = () => {
   const { setPreloader } = useContextProvider();
   const loaderRef = useRef(null);
   const containerRef = useRef(null);
-  const counter1 = useRef(null);
-  const counter2 = useRef(null);
-  const counter3 = useRef(null);
-
-  const count3Num = Array.from({ length: 29 }, (_, i) => i % 10);
-  count3Num.push(0);
+  const vidRef = useRef(null);
 
   useEffect(() => {
     const tl = gsap.timeline();
-    function animate(counter, duration, delay = 0) {
-      const numHeight = counter.querySelector(".num").clientHeight;
-      const totalDistance =
-        (counter.querySelectorAll(".num").length - 1) * numHeight;
-
-      gsap.to(counter, {
-        y: -totalDistance,
-        duration: duration,
-        delay: delay,
-        ease: "power2.inOut",
-      });
-    }
-
-    animate(counter3.current, 5);
-    animate(counter2.current, 6);
-    animate(counter1.current, 2, 4);
 
     tl.to(loaderRef.current.querySelector(".loader_bar"), {
       width: "100%",
       duration: 5,
       ease: "power2.inOut",
+      onUpdate: function () {
+        if (tl.progress() >= 0.55) {
+          vidRef.current.play();
+        }
+      },
     })
-      .to(".digit", {
-        top: "-150px",
-        stagger: {
-          amount: 0.25,
-        },
-        delay: 1,
-        duration: 1,
-        ease: "power4.inOut",
-      })
       .to(
         loaderRef.current,
         {
           opacity: 0,
         },
-        "<.5"
+        "-=.5"
       )
+      .to(vidRef.current, {
+        delay: 2,
+        opacity: 0,
+      })
       .to(containerRef.current, {
         yPercent: -100,
-        onComplete: () => setPreloader(false),
+        duration: 0.5,
+        ease: "power2.inOut",
+        onUpdate: function () {
+          if (tl.progress() >= 0.95) {
+            setPreloader(false);
+          }
+        },
       });
   }, []);
 
@@ -147,32 +109,7 @@ const Preloader = () => {
       <Loader ref={loaderRef}>
         <div className="loader_bar"></div>
       </Loader>
-      <div className="counter">
-        <div ref={counter1} className="digit">
-          <div className="num">0</div>
-          <div className="num num1offset1">1</div>
-        </div>
-        <div ref={counter2} className="digit">
-          <div className="num">0</div>
-          <div className="num num1offset2">1</div>
-          <div className="num">2</div>
-          <div className="num">3</div>
-          <div className="num">4</div>
-          <div className="num">5</div>
-          <div className="num">6</div>
-          <div className="num">7</div>
-          <div className="num">8</div>
-          <div className="num">9</div>
-          <div className="num">0</div>
-        </div>
-        <div ref={counter3} className="digit">
-          {count3Num.map((number, index) => (
-            <div key={index} className="num">
-              {number}
-            </div>
-          ))}
-        </div>
-      </div>
+      <video ref={vidRef} src={LogoAni} muted></video>
     </Container>
   );
 };
